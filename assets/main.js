e4ed09359caa96aa3902d2ab15b0f271d4a6b59e
Jarvis <jarvisvip@gmail.com>
2016-12-14 17:17:43 +0800

    The Build
    Satisfy the list of requirements below, commit, and push your code to
    GitHub.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
-  // your code will go here
+   $.ajax({
+    url: 'https://www.codeschool.com/users/2802883.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var completed = response.courses.completed;
+      $.each(completed, function(key, obj)
+      {
+      	var course = $('<div></div>');
+      	course.addClass("course");
+      	var title = $('<h3>' + obj.title + '</h3>');
+      	var image = $("<img src='" + obj.badge + "'/>");
+      	var button = $("<a href='" + obj.url + "'>See Course</a>")
+      	button.addClass("btn").addClass("btn-primary");
+      	button.attr( "target", "_blank" );
+      	course.append(title, image, button);
+       	$("#badges").append(course);
+      });
+    }
+  });
 
 });
